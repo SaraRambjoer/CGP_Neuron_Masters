bb8e59b4f161eb91bb2a57be9ae05f85493b5d6e
Jon Oddvar Rambjør <44160493+jonoddram@users.noreply.github.com>
2021-10-13 16:16:53 +0200

    Initial project

---
+++ Neuron Engine/engine.py
@@ -0,0 +1,1 @@
+# here define the world space
---
+++ Neuron Engine/neuron.py
@@ -0,0 +1,23 @@
+
+class Neuron():
+    def __init__(self) -> None:
+        self.move_func = None
+        # other functions here
+        self.somas = []
+        self.dendrites = []
+    
+    # functions defining neuron-level behaviour based on cgp program output, also preparing input for them
+
+class Synapse():
+    def __init__(self) -> None:
+        pass
+    
+    # similar to neuron
+
+class Dendrite():
+    def __init__(self) -> None:
+        pass
+    
+    # similar to neuron
+
+

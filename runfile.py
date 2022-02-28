41150e0f20e082aef232ca4a47f9b8469d71f2e3
Jon Oddvar Rambjør <jonoddram@gmail.com>
2022-02-28 17:01:00 +0100

    fixes for array jobs IDUN

--- runfile.py
+++ runfile.py
@@ -6,6 +6,11 @@ if __name__ == "__main__":
     config_filename = sys.argv[1]
     output_path = sys.argv[2]
     if sys.argv[3]:
+        output_path = os.path.join(output_path, config_filename)
+        if not os.path.exists(output_path):
+            os.mkdir(output_path)
         output_path = os.path.join(output_path, str(sys.argv[3]))
-        os.mkdir(output_path)
+        if not os.path.exists(output_path):
+            os.mkdir(output_path)
+
     runme(config_filename, output_path)

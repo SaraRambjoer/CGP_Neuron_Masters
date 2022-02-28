16c34d028cdf7480ba74c4c1e0614883fc8d163d
Jon Oddvar Rambjør <jonoddram@gmail.com>
2022-02-28 16:46:38 +0100

    array job support (optional)

--- runfile.py
+++ runfile.py
@@ -1,7 +1,11 @@
 from MainController import runme
 import sys
+import os
 
 if __name__ == "__main__":
     config_filename = sys.argv[1]
     output_path = sys.argv[2]
+    if sys.argv[3]:
+        output_path = os.path.join(output_path, str(sys.argv[3]))
+        os.mkdir(output_path)
     runme(config_filename, output_path)

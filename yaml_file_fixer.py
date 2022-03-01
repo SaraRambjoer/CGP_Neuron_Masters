eae15d0c93ba49d160efef03b663e6d5f4b3137d
Jon Oddvar Rambjør <jonoddram@gmail.com>
2022-03-01 12:56:15 +0100

    iteratino count fix

--- yaml_file_fixer.py
+++ yaml_file_fixer.py
@@ -1,15 +1,15 @@
 from unittest import skip
 
-filepath = "D:\jonod\masters\CGP_Neuron_masters_idun\log_config_complex_4_1645711657.5639818\statistics.yml"
+filepath = r"C:\Users\jonora\Documents\CGP Neuron Masters\statistics.yml"
 
 output = []
-first_iterations_found = True
+iterations_found = 0
 with open(filepath, "r") as f:
     for line in f:
         if "iterations:" in line:
-            if first_iterations_found:
+            if iterations_found < 2:
                 output.append(line)
-                first_iterations_found = False
+                iterations_found += 1
         else:
             output.append(line)
 

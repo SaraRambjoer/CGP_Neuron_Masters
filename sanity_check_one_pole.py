693dfe3f4464a6fc90de452d3fe0cd1c6e003a8f
Jon Oddvar Rambjør <jonoddram@gmail.com>
2022-03-24 13:13:19 +0100

    added std check

--- sanity_check_one_pole.py
+++ sanity_check_one_pole.py
@@ -2,6 +2,7 @@ from os import environ
 import one_pole_problem
 from HelperClasses import randchoice
 from matplotlib import pyplot as plt
+import numpy as np
 
 
 runs = []
@@ -28,7 +29,40 @@ val = 0
 for num in range(100):
     for fitness_list in runs:
         val += fitness_list[num]
-    
+  
 print("Expected random fitness: ", val/(runs_total*environment_timesteps_per_run))
 
 # expect 0.101643
+
+fitnessess_flat = [np.average(x_inner) for x_inner in runs]
+print("Standard deviation of fitness: ", np.std(fitnessess_flat), "Average: ", np.average(fitnessess_flat))
+
+
+
+
+avg = 0.10167700000000003
+std = 0.0037520222547314385
+
+fitnessess_flat = np.asarray(fitnessess_flat)
+better_vals = [0.01, 0.02, 0.05, 0.08, 0.102]
+for val in better_vals:
+    derived_val = avg-val
+    prob_singular = 1.0-len(fitnessess_flat[fitnessess_flat <= derived_val])/len(fitnessess_flat)
+    prob = 1.0-prob_singular*prob_singular*prob_singular*prob_singular
+    print(prob)
+
+gauss_samples_better = []
+for num in range(runs_total):
+    samples = np.random.normal(avg, std, 4)
+    gauss_samples_better.append(
+        [
+            len(samples[samples <= avg - better_vals[0]])/4,
+            len(samples[samples <= avg - better_vals[1]])/4,
+            len(samples[samples <= avg - better_vals[2]])/4,
+            len(samples[samples <= avg - better_vals[3]])/4
+        ]
+    )
+
+print(np.average(gauss_samples_better, 0))
+
+
